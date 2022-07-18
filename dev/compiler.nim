89c30d9328c445a3bbb61bf7813ca1439b4b757e
lafixier <100778884+lafixier@users.noreply.github.com>
2022-07-18 22:13:04 +0900

    feat(dev/compiler.nim): added compiler

---
+++ dev/compiler.nim
@@ -0,0 +1,23 @@
+import types
+import lexer
+import parser
+
+
+proc read_file(file_path: string): string =
+  var f: File = open(file_path, FileMode.fmRead)
+  defer:
+    close(f)
+  result = f.readAll()
+
+proc compile(src: string) =
+  let tokens: seq[string] = lex(src)
+  let ast: Ast = parse(tokens)
+  echo tokens
+  # echo ast
+
+proc main() =
+  let src = read_file("./dev/hello_world.relsis")
+  compile(src)
+
+
+main()
